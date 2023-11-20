7d91b55c20b27eaf15eb561f88176cb20bfc39e2
Nathan De Pachtere <nathan.depachtere@alpsify.com>
2023-11-20 18:30:22 +0100

    Improve example

---
+++ packages/package-c/CatCar.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>Cat Car</div>
+	<car-component />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+const CarComponent = defineAsyncComponent(() => import('@test/package-b/Car.vue'));
+</script>
--- projects/project-a/PageHomeA.vue
+++ projects/project-a/PageHomeA.vue
@@ -1,5 +1,15 @@
 <template>
 	<div>Home A</div>
+	<car-component />
+	<bus-component />
+	<dog-bus-component />
+	<cat-car-component />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+const CarComponent = defineAsyncComponent(() => import('@test/package-b/Car.vue'));
+const BusComponent = defineAsyncComponent(() => import('@test/package-b/Bus.vue'));
+const DogBusComponent = defineAsyncComponent(() => import('@test/package-c/DogBus.vue'));
+const CatCarComponent = defineAsyncComponent(() => import('@test/package-c/CatCar.vue'));
+</script>
